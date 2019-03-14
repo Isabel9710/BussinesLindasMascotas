d24a74696117faa8ee73ef5b834a001b530e9b78
Isabel Medina <medisabel97@gmail.com>
2019-03-13 20:28:43 -0500

    Se completa el AuthoFilter

--- BussinesLindasMascotas/src/main/java/co/com/bussineslm/filters/AuthFilter.java
+++ BussinesLindasMascotas/src/main/java/co/com/bussineslm/filters/AuthFilter.java
@@ -1,9 +1,15 @@
 
 package co.com.bussineslm.filters;
 
+import co.com.bussineslm.JPAcontrollers.UsuariosJpaController;
+import co.com.bussineslm.entities.Usuarios;
+import co.com.bussineslm.utilities.Encrypter;
+import co.com.bussineslm.utilities.JPAFactory;
+import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jwt.JWTClaimsSet;
 import java.io.IOException;
 import java.security.Principal;
+import java.text.ParseException;
 import javax.annotation.Priority;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
@@ -14,6 +20,7 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.ext.Provider;
+import org.joda.time.DateTime;
 
 @Provider
 @Priority(Priorities.AUTHENTICATION)
@@ -44,7 +51,35 @@ public class AuthFilter implements ContainerRequestFilter, ContainerResponseFilt
 
             try {
                 
-            } catch (Exception e) {
+                claims = (JWTClaimsSet) AuthDigest.decodeToken(token);
+                
+            } catch (ParseException e) {
+                throw new IOException("Ha ocurrido un error al intentar validar el token.");
+            } catch (JOSEException e) {
+                throw new IOException("El token es invalido.");
+            }
+            
+            if(new DateTime(claims.getExpirationTime()).isBefore(DateTime.now())){
+               co.com.bussineslm.utilities.Response res = new co.com.bussineslm.utilities.Response();
+               res.setStatus(false);
+               res.setMessage("Credenciales Necesarias.");
+               
+               request.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity(res).build()); 
+            }else{
+                UsuariosJpaController ctrl = new UsuariosJpaController(JPAFactory.getFACTORY());
+                
+                try {
+                    Usuarios usuario = ctrl.findUsuarios(Encrypter.decrypt(claims.getSubject()));
+                    
+                    if(usuario != null){
+                        Authorizer authorizer = new Authorizer(usuario.getIdUsuario().getIdentificacion().toString(), true, originalContext.isSecure());
+                        request.setSecurityContext(authorizer);
+                    } else{
+                        throw new IOException("Ha ocurrido un error al intentar validar el token.");
+                    }
+                } catch (Exception e) {
+                    throw new IOException("Ha ocurrido un error al intentar validar el token.");
+                }
             }
         }
     }
